fceac9e8ad3421e6baaadc9b1e5d2eff5759d9b2
RocioMarquez92 <65229340+RocioMarquez92@users.noreply.github.com>
2026-02-18 05:36:33 +0000

    include our codespace name for the URL endpoint

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
 from octofit_tracker import views
+import os
+
 
 
 router = DefaultRouter()
@@ -26,6 +29,13 @@ router.register(r'teams', views.TeamViewSet, basename='team')
 router.register(r'activities', views.ActivityViewSet, basename='activity')
 router.register(r'workouts', views.WorkoutViewSet, basename='workout')
 router.register(r'leaderboard', views.LeaderboardViewSet, basename='leaderboard')
+
+# Construir la URL base para Codespaces
+codespace_name = os.environ.get('CODESPACE_NAME')
+if codespace_name:
+    base_url = f"https://{codespace_name}-8000.app.github.dev"
+else:
+    base_url = "http://localhost:8000"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
